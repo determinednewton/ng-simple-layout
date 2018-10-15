bef9c540c0bacc72a73e081726dccfb3ac5f7e8f
Robert Todea <robert.todea@gmail.com>
2018-10-15 22:48:57 +0300

    Creating 2 modules:
    1. core for header and footer
    2. shared for image widget
    
    ```
    [app] -> [BodyComponent]
    [core] -> [HeaderComponent, FooterComponent]
    [shared] -> [ImageComponent]
    
    [app] -> [core], [shared]
    [core] -> [shared]
    ```
    
    Discussed about the need for exports vs only declaring components.
    Always be aware of the module context.
    WebStorm helps with not having a component properly imported.

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageComponent } from './image/image.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ImageComponent],
+  exports: [ImageComponent]
+})
+export class SharedModule { }
